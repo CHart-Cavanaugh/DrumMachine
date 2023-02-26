b09a72b9399f856657ec278200854f20ab6fd950
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-02-26 10:50:36 -0800

    Renamed MIN_VOLUME and MAX_VOLUME to minVolume and maxVolume, respectively.

--- src/components/MachineSettings.js
+++ src/components/MachineSettings.js
@@ -3,8 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { changeVolume } from '../app/slices/volumeSlice';
 
 const MachineSettings = props => {
-  const MIN_VOLUME = useSelector((state) => state.volume.value.minVolume);
-  const MAX_VOLUME = useSelector((state) => state.volume.value.maxVolume);
+  const minVolume = useSelector((state) => state.volume.value.minVolume);
+  const maxVolume = useSelector((state) => state.volume.value.maxVolume);
   const dispatch = useDispatch();
   const initialVolumeStep = 5;
   const volumeSteps = 10;
@@ -20,10 +20,10 @@ const MachineSettings = props => {
       <input
         id="volume-setting"
         type="range"
-        min={MIN_VOLUME * 100}
-        max={MAX_VOLUME * 100}
+        min={minVolume * 100}
+        max={maxVolume * 100}
         onChange={handleChange}
-        step={MAX_VOLUME * volumeSteps}
+        step={maxVolume * volumeSteps}
       />
     </label>
 
@@ -31,7 +31,7 @@ const MachineSettings = props => {
 
   useEffect(() => {
     let volumeSetting = document.getElementById("volume-setting");
-    volumeSetting.value = MAX_VOLUME * volumeSteps * initialVolumeStep;
+    volumeSetting.value = maxVolume * volumeSteps * initialVolumeStep;
     dispatch(changeVolume({
       volType: "current",
       newVolume: document.getElementById("volume-setting").value / 100
